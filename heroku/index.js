e7b4afd155fc9ee48538c141591a82b65fca612f
Ajinkya Sanjay Patil <95755204+Ajinkyasp99@users.noreply.github.com>
2022-10-20 17:52:24 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -14,9 +14,8 @@ var xhub = require('express-x-hub');
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-app.use(bodyParser.json());
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
-
+app.use(bodyParser.json());
 
 var token = process.env.TOKEN || 'token';
 var received_updates = [];
